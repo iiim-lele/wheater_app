e3d2495e22215d53a8a43911fb8d0b2c00ebaae1
iiim-lele <leticia.g.souza9@aluno.senai.br>
2025-09-29 08:46:59 -0300

    arquivos iniciais

---
+++ tempo.py
@@ -0,0 +1,9 @@
+import flet as ft
+import httpx
+from datetime import datetime
+
+API_KEY = "05f4ee67848cc2f2685db6e4c1cf5f3e"
+
+# ===================== ESQUEMAS DE COR =====================
+
+# Digitar (a partir deste ponto)
